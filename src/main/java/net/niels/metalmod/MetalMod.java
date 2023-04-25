af7ea393f5e30ea9dab54f7315b2ccd746d92cb8
BuildTools <unconfigured@null.spigotmc.org>
2023-04-25 17:48:01 +0200

    Added Voidsteel Blocks and ore

--- src/main/java/net/niels/metalmod/MetalMod.java
+++ src/main/java/net/niels/metalmod/MetalMod.java
@@ -8,6 +8,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.niels.metalmod.block.ModBlocks;
 import net.niels.metalmod.item.ModItems;
 import org.slf4j.Logger;
 
@@ -23,6 +24,7 @@ public class MetalMod
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
